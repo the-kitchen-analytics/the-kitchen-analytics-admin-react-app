dc20c79d3f6776d04e8cee158dbbc02e680ff1c8
Maxim Marus <alexandrovichmm@gmail.com>
2022-12-17 20:17:53 +0300

    Add semantic-ui-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+import App from './components/App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
